8cd0ab3238f20618eaf26e8f902b4e9983e99a7e
entropy-sumit <sumitr600@gmail.com>
2022-01-23 18:45:54 +0530

    [Add] sumit added UC1TEST program

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -6,5 +6,20 @@ namespace CabInvoiceGenartorTest
     [TestClass]
     public class UnitTest1
     {
+        InvoiceGeneartor invoiceGeneartor = null;
+
+        [TestMethod]
+        public void GivenDistanceAndTimeShouldTotalFare()
+        {
+            invoiceGeneartor = new InvoiceGeneartor(RideType.Normal);
+            double distance = 2.0;
+            int time = 5;
+            double fare = invoiceGeneartor.CalcaulateFare(distance, time);
+            double expected = 25;
+
+            Assert.AreEqual(expected, fare);
+
+        }
+
     }
 }
